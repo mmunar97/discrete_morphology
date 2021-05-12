9a1d0f7f00cc5d631e7b1787c63bf8b0c95ec836
mmunar97 <mmunar97@gmail.com>
2021-05-12 13:13:09 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='discrete_fuzzy_morphology',
+    version='1.0',
+    packages=['discrete_fuzzy_morphology'],
+    url='https://github.com/mmunar97/discrete-fuzzy-morphology',
+    license='mit',
+    author='marcmunar',
+    author_email='marc.munar@uib.es',
+    description='Set of algorithms for image processing using fuzzy operators',
+    include_package_data=True,
+    install_requires=[
+        "discrete_fuzzy_operators"
+    ]
+)
